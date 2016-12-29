0ca77228c9ce62975cea230b020ef615bfc5c219
sridh <sridharjajoo@gmail.com>
2016-12-29 15:17:04 +0530

    Displaying beacon namespace and InstanceId

--- app/src/main/java/com/example/sridh/inquisitenerds/MainActivity.java
+++ app/src/main/java/com/example/sridh/inquisitenerds/MainActivity.java
@@ -16,49 +16,85 @@ import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
 import java.util.Collection;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer {
-    protected static final String TAG = "RangingActivity";
-    private BeaconManager beaconManager;
-    private TextView textView;
+import static org.altbeacon.beacon.service.BeaconService.TAG;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+
+    private BeaconManager mBeaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the main Eddystone-UID frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        beaconManager.bind(this);
+        mBeaconManager.bind(this);
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
-    }
-    @Override
-    public void onBeaconServiceConnect() {
-        beaconManager.addRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                final Collection<Beacon> beacons1=beacons;
 
-                if (beacons.size() > 0) {
-                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
-                    //textView.setText(region.getUniqueId() + "");
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            // startActivity(new Intent(MainActivity.this,Main2Activity.class));
-                            ((TextView) MainActivity.this.findViewById(R.id.text_view)).setText("The first beacon I see is about "+ beacons1.iterator().next().getDistance()+" meters away.");
-                        }});}}});
-        try
-        {
-            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        }
+
+
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.addRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        final Collection<Beacon> beacons1=beacons;
+
+        for (Beacon beacon : beacons) {
+      //      if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+                // This is a Eddystone-UID frame
+            Identifier namespaceId = beacon.getId1();
+            final String namespace =namespaceId.toString();
+            Log.d(TAG, "String I see a beacon transmitting namespace id: " + namespace );
+
+
+            final Identifier instanceId = beacon.getId2();
+            final String instance =instanceId.toString();
+            if(beacons.size()>0){
+            if(namespace.compareTo("0x996a4b276250c5a82ba4")==0 && instance.compareTo("0x000000000000")==0) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        // startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                        ((TextView) MainActivity.this.findViewById(R.id.text_view)).setText("The beacon detected has namespaceId = " +namespace
+                        + "You are at the first beacon");}});}
+                }
+
+
+            if(namespace.compareTo("0x524cba7394a8fb191969")==0 && instance.compareTo("0x000000000000")==0) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        // startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                        ((TextView) MainActivity.this.findViewById(R.id.text_view)).setText("The beacon detected has namespaceId = " +namespace
+                               + "You are at the second beacon");}});}
+        }
+
+
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
     }
 }
