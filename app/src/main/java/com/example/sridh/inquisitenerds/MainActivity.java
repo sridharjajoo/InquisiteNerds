8e010c61f570c0a58a7e92f3ca9271566b7dc3ce
sridh <sridharjajoo@gmail.com>
2016-12-29 12:39:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sridh/inquisitenerds/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.sridh.inquisitenerds;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+    protected static final String TAG = "RangingActivity";
+    private BeaconManager beaconManager;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        beaconManager.bind(this);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                final Collection<Beacon> beacons1=beacons;
+
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    //textView.setText(region.getUniqueId() + "");
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            // startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                            ((TextView) MainActivity.this.findViewById(R.id.text_view)).setText("The first beacon I see is about "+ beacons1.iterator().next().getDistance()+" meters away.");
+                        }});}}});
+        try
+        {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
+}
